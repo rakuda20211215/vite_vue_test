eea536a3f813837ec3d1f4df90d270e68f740a8e
makanai_sub <nonoise.zero@gmail.com>
2023-07-29 08:49:03 +0900

    deskpc

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  proseWrap: "preserve",
+  trailingComma: "all",
+  // あなたの素晴らしいルール
+};
